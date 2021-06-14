12342c8ea313f3ad84b48a2a9498c2308e78f14d
Oleksandr Dievri <o.dievri@gmail.com>
2021-06-14 16:07:42 +0300

    fix currency

--- xdr/xdr_generated.go
+++ xdr/xdr_generated.go
@@ -4438,7 +4438,7 @@ var (
 //        uint32 ledgerSeq; // sequence number of this ledger
 //
 //        int64 totalCoins; // total number of nibbs in existence.
-//                          // 10,000,000 nibbs in 1 XLM
+//                          // 10,000,000 nibbs in 1 XDB
 //
 //        int64 feePool;       // fees burned since last inflation run
 //        uint32 inflationSeq; // inflation sequence number
